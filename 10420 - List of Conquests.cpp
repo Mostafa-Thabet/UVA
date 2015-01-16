e58318d703e9206de6919b77172350ccf0990699
Mostafa-Thabet <mostafathabet.94@gmail.com>
2015-01-16 18:54:21 +0200

    Update 10420 - List of Conquests.cpp

--- 10420 - List of Conquests.cpp
+++ 10420 - List of Conquests.cpp
@@ -1,1 +1,27 @@
-10420 - List of Conquests.cpp
+/*
+ * 10420 - List of Conquests.cpp
+ *
+ *  Created on: Jan 16, 2015
+ *      Author: Mostafa
+ */
+
+#include<bits/stdc++.h>
+
+using namespace std; 
+
+int main () {
+	int n;
+	cin>>n;
+	map<string,int> m;
+
+	for(int i=0;i<n;i++) {
+		string s;
+		cin>>s;
+		m[s]++;
+		getline(cin,s);
+	}
+	for(map<string,int>::iterator it=m.begin();it!=m.end();it++) {
+		cout<<it->first<<" "<<it->second<<endl;
+	}
+	return 0;
+}
